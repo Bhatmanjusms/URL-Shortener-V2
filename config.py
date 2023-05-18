a07c082fadda8fc40b9e16d547abc46d5a999ad7
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2023-05-18 07:43:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,26 +16,26 @@ def is_enabled(value, default):
 
 # Mandatory variables for the bot to start
 # API ID from https://my.telegram.org/auth
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID", "977080"))
 # API Hash from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Bot token from @BotFather
 ADMINS = (
-    [int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
+    [int(i.strip()) for i in os.environ.get("ADMINS", "399726799").split(",")]
     if os.environ.get("ADMINS")
     else []
 )
 
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertor")
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL", None
+    "DATABASE_URL", "mongodb+srv://Filmyfunda:<password>@cluster0.90xpiei.mongodb.net/?retryWrites=true&w=majority"
 )  # mongodb uri from https://www.mongodb.com/
-OWNER_ID = int(os.environ.get("OWNER_ID"))  # id of the owner
+OWNER_ID = int(os.environ.get("OWNER_ID", "399726799"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
 #  Optionnal variables
 LOG_CHANNEL = int(
-    os.environ.get("LOG_CHANNEL", "0")
+    os.environ.get("LOG_CHANNEL", "-1001896983424")
 )  # log channel for information about users
 UPDATE_CHANNEL = os.environ.get(
     "UPDATE_CHANNEL", False)  # For Force Subscription
@@ -43,7 +43,7 @@ BROADCAST_AS_COPY = is_enabled(
     (os.environ.get("BROADCAST_AS_COPY", "False")), False
 )  # true if forward should be avoided
 IS_PRIVATE = is_enabled(
-    os.environ.get("IS_PRIVATE", "False"), "False"
+    os.environ.get("IS_PRIVATE", "True"), "False"
 )  # true for private use and restricting users
 SOURCE_CODE = os.environ.get(
     "SOURCE_CODE", "https://github.com/kevinnadar22/URL-Shortener-V2"
@@ -51,15 +51,15 @@ SOURCE_CODE = os.environ.get(
 # image when someone hit /start
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "")
 LINK_BYPASS = is_enabled(
-    (os.environ.get("LINK_BYPASS", "False")), False
+    (os.environ.get("LINK_BYPASS", "True")), False
 )  # if true, urls will be bypassed
 # your shortener site domain
-BASE_SITE = os.environ.get("BASE_SITE", "droplink.co")
+BASE_SITE = os.environ.get("BASE_SITE", "viplinks.io")
 
 # For Admin use
-CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
+CHANNELS = is_enabled((os.environ.get("CHANNELS", "False")), True)
 CHANNEL_ID = (
-    [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")]
+    [int(i.strip()) for i in os.environ.get("CHANNEL_ID",).split(" ")]
     if os.environ.get("CHANNEL_ID")
     else []
 )
